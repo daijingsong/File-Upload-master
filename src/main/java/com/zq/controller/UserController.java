035b702a5e9731f31de10c3a4b6a391dd3f5cd9b
“dai” <15004785909@163.com>
2022-07-19 16:13:12 +0800

    第1次提交hot-fix

--- src/main/java/com/zq/controller/UserController.java
+++ src/main/java/com/zq/controller/UserController.java
@@ -30,6 +30,7 @@ public class UserController {
                         RedirectAttributes attributes) {
 
         log.info("接收到的数据是username:{},password:{}",username,password);
+        System.out.println("我是hotfix文件");
 
         //获取当前的用户
         Subject subject = SecurityUtils.getSubject();
